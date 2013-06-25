fd8258848ebaacbfb5c0f52d70af19ccf771c71e
az37 <zeier.alex@gmail.com>
2013-06-26 00:11:25 +0200

    init commit

---
+++ StatsOn/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("StatsOn.mod")]
+[assembly: AssemblyDescription("always show unit stats in game")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Epix")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
---
+++ StatsOn/StatsOn.cs
@@ -0,0 +1,87 @@
+using System.Reflection;
+using Mono.Cecil;
+using ScrollsModLoader.Interfaces;
+
+namespace StatsOn.mod
+{
+    public class StatsOn : BaseMod
+    {
+        private BattleMode _battleMode;
+        private int _init;
+        private int _lastTurn;
+        private FieldInfo _showStatsInfo;
+
+
+        public static string GetName()
+        {
+            return "NoMoCtrl";
+        }
+
+        public static int GetVersion()
+        {
+            return 1;
+        }
+
+        public static MethodDefinition[] GetHooks(TypeDefinitionCollection scrollsTypes, int version)
+        {
+            try
+            {
+                return new[]
+                    {
+                        scrollsTypes["BattleMode"].Methods.GetMethod("Update")[0]
+                    };
+            }
+            catch
+            {
+                return new MethodDefinition[] {};
+            }
+        }
+
+
+        public override bool BeforeInvoke(InvocationInfo info, out object returnValue)
+        {
+            returnValue = null;
+            return false;
+        }
+
+        public override void AfterInvoke(InvocationInfo info, ref object returnValue)
+        {
+            if (info.targetMethod.Equals("Update"))
+            {
+                if (_battleMode == null)
+                {
+                    _battleMode = (BattleMode) info.target;
+                }
+
+                //the first update happens before the game is done initializing, causing it the stats to look weird until after the first refresh
+                if (_init > 10)
+                {
+                    _showStatsInfo = typeof (BattleMode).GetField("showUnitStats",
+                                                                  BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    if (_showStatsInfo != null && !(bool) _showStatsInfo.GetValue(_battleMode))
+                    {
+                        typeof (BattleMode).GetMethod("toggleUnitStats", BindingFlags.NonPublic | BindingFlags.Instance)
+                                           .Invoke(_battleMode, null);
+                    }
+
+
+                    //units being attacked seems to cause the stats for that unit to turn off (but showUnitStats is still true)
+                    //so setting showUnitStats to false at the beginning of each turn, causing an update for all of them, seems to be the easiest fix
+
+                    FieldInfo _turnInfo = typeof (BattleMode).GetField("currentTurn",
+                                                                       BindingFlags.NonPublic | BindingFlags.Instance);
+                    var turn = (int) _turnInfo.GetValue(_battleMode);
+
+                    if (turn > _lastTurn)
+                    {
+                        _showStatsInfo.SetValue(_battleMode, false);
+                        _lastTurn = turn;
+                    }
+                }
+                else
+                    _init++;
+            }
+        }
+    }
+}
